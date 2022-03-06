47a596bd0da95c57452d135dd2e0d3e699dfa5e5
Abdel Ghani Muhra <agmuhra@gmail.com>
2022-03-06 16:27:21 +0200

    completed all the questions

--- Assignment 1.R
+++ Assignment 1.R
@@ -2,6 +2,7 @@ library("tidyverse")
 
 # a) read dataset into a variable
 videos <- read_csv("videos.csv")
+
 # b)
 avg_file_size <- mean(videos$`File Size`)
 print(avg_file_size)
@@ -31,21 +32,39 @@ if (song_length_shapirotest$p.value < 0.05) {
   print('normal')
 }
 
-# d) 
-# avg_file_size_high_quality <- avg_file_size[videos$`Video Quality` == 'High']
-# avg_file_size_low_quality <- avg_file_size[videos$`Video Quality` == 'Low']
-# t.test(x= avg_file_size[videos$`Video Quality` == 'High'],
-#        y= avg_file_size[videos$`Video Quality` == 'Low'])
-
-
-
-
-
-
-
-# EXTRA below i made a plot comparing the size and length of songs
+# d) and f) using t-test to compare the averages of the size and length of songs
 #with 'high' & 'low' video quality;
 
+file_size_high_quality <- videos$`File Size`[videos$`Video Quality` == 'High']
+file_size_low_quality <- videos$`File Size`[videos$`Video Quality` == 'Low']
+t.test(x= avg_file_size_high_quality, y = avg_file_size_low_quality)
+
+song_length_high_quality <- videos$`Song Length`[videos$`Video Quality` == 'High']
+song_length_low_quality <- videos$`Song Length`[videos$`Video Quality` == 'Low']
+t.test(x = song_length_high_quality, y = song_length_low_quality)
+# using the F-test to compare the standard deviations of the size and 
+#length of songs with 'high' & 'low' video quality;
+var.test(file_size_high_quality,file_size_low_quality)
+var.test(song_length_high_quality,song_length_low_quality)
+
+# e) boxplot to compare the averages of the size and length of songs
+#with 'high' & 'low' video quality;
+ggplot(data = videos,
+       filter(videos$`Video Quality` %in% c('High','Low')),
+       mapping = aes(x = videos$`File Size`,
+                     color = videos$`Video Quality`)) +
+  geom_boxplot() +
+  labs(color = "Video Quality", x = "File Size (MB)")
+
+# g) scatter plot of files sizes and song lengths
+ggplot(data = videos,
+       mapping = aes(x = videos$`File Size`, y = videos$`Song Length`)) +
+  geom_point(alpha = 0.5, size = 2) +
+  labs(y = "Song Length (Seconds)", x = "File Size (MB)")
+
+
+# h) scatter plot of files sizes and song lengths for high quality and low
+# quality videos
 ggplot(data = videos,
        filter(videos$`Video Quality` %in% c('High','Low')),
        mapping = aes(x = videos$`File Size`, y = videos$`Song Length`,
@@ -53,20 +72,3 @@ ggplot(data = videos,
   geom_point(alpha = 0.5, size = 2) +
   labs(y = "Song Length (Seconds)", x = "File Size (MB)",
        color = "Video Quality")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
